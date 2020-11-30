257d3d46e88cde8c15692ed239e5c596bd1b791c
mathisschmidt <73819381+mathisschmidt@users.noreply.github.com>
2020-11-30 09:37:41 +0100

    rt

---
+++ realisation/JS/tp_ServiceWorker/sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/tp_ServiceWorker/',
+          '/tp_ServiceWorker/tp_serviceWorker.html',
+          'https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+      // caches.match() always resolves
+      // but in case of success response will have value
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          // response may be used only once
+          // we need to save clone to put one copy in cache
+          // and serve second one
+          let responseClone = response.clone();
+          
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          return caches.match('/sw-test/gallery/myLittleVader.jpg');
+        });
+      }
+    }));
+  });
+  
